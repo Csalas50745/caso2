ddb77e4d23c5cc4a3ea954e1d1e302a337ff0573
Carlos Salas Alpizar <141593436+Csalas50745@users.noreply.github.com>
2023-12-06 19:24:33 -0600

    Update Grupo.java

--- CasodeEstudios2/src/casodeestudios2/Grupo.java
+++ CasodeEstudios2/src/casodeestudios2/Grupo.java
@@ -9,5 +9,44 @@ package casodeestudios2;
  * @author charliesalas
  */
 public class Grupo {
-    
+
+Movimiento[] movimientos;
+
+public Grupo(int tamaño) {
+ movimientos = new Movimiento[tamaño];
+
+class Movimiento {
+
+   String nombre; 
+   double monto; 
+
+   public Movimiento(String nombre, double monto) {
+   this.nombre = nombre;
+   this.monto = monto;
+            }
+        }
+    }
+
+    public void agregarMovimiento(String nombre, double monto) {
+        for (int i = 0; i < movimientos.length; i++) {
+            if (movimientos[i] == null) {
+                movimientos[i] = new Movimiento(nombre, monto);
+                break;
+            }
+        }
+    }
+
+    public void calcularSaldoTotal() {
+        String nombre = movimientos[0].nombre;
+        double total = 0;
+
+        for (int i = 0; i < movimientos.length; i++) {
+            if (movimientos[i] != null && movimientos[i].nombre.equals(nombre)) {
+                total += movimientos[i].monto;
+            }
+        }
+
+        System.out.println("El total de deudas de " + nombre + " es de: " + total + " dolares.");
+    }
 }
+    
